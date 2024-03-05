b608c95ca4c33d1d4415034f16b5ea2637e125f2
Arnau <61731879+0xArnau@users.noreply.github.com>
2024-03-05 10:27:56 +0100

    fix: change the way the menu is done

--- lib/src/home/views/home_view.dart
+++ lib/src/home/views/home_view.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:transsectes_app/src/home/views/menu_view.dart';
+import 'package:transsectes_app/src/utils/colors.dart';
 
 class HomeView extends StatefulWidget {
   const HomeView({super.key});
@@ -10,100 +11,26 @@ class HomeView extends StatefulWidget {
   State<HomeView> createState() => _HomeViewState();
 }
 
-class _HomeViewState extends State<HomeView>
-    with SingleTickerProviderStateMixin {
-  late AnimationController _drawerSlideController;
-
-  @override
-  void initState() {
-    super.initState();
-
-    _drawerSlideController = AnimationController(
-      vsync: this,
-      duration: const Duration(milliseconds: 150),
-    );
-  }
-
-  @override
-  void dispose() {
-    _drawerSlideController.dispose();
-    super.dispose();
-  }
-
-  bool _isDrawerOpen() {
-    return _drawerSlideController.value == 1.0;
-  }
-
-  bool _isDrawerOpening() {
-    return _drawerSlideController.status == AnimationStatus.forward;
-  }
-
-  bool _isDrawerClosed() {
-    return _drawerSlideController.value == 0.0;
-  }
-
-  void _toggleDrawer() {
-    if (_isDrawerOpen() || _isDrawerOpening()) {
-      _drawerSlideController.reverse();
-    } else {
-      _drawerSlideController.forward();
-    }
-  }
-
+class _HomeViewState extends State<HomeView> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Colors.white,
+      drawer: MenuView(),
+      backgroundColor: kColorBackground,
       appBar: _buildAppBar(),
-      body: Stack(
-        children: [
-          _buildDrawer(),
-        ],
+      body: ListView(
+        children: [],
       ),
     );
   }
 
   PreferredSizeWidget _buildAppBar() {
     return AppBar(
+      backgroundColor: kColorBackground,
+      foregroundColor: kColorTitle,
       title: const Text(
         'Transsectes APP',
-        style: TextStyle(
-          color: Colors.black,
-        ),
       ),
-      elevation: 0.0,
-      automaticallyImplyLeading: false,
-      actions: [
-        AnimatedBuilder(
-          animation: _drawerSlideController,
-          builder: (context, child) {
-            return IconButton(
-              onPressed: _toggleDrawer,
-              icon: _isDrawerOpen() || _isDrawerOpening()
-                  ? const Icon(
-                      Icons.clear,
-                      color: Colors.black,
-                    )
-                  : const Icon(
-                      Icons.menu,
-                      color: Colors.black,
-                    ),
-            );
-          },
-        ),
-      ],
-    );
-  }
-
-  Widget _buildDrawer() {
-    return AnimatedBuilder(
-      animation: _drawerSlideController,
-      builder: (context, child) {
-        return FractionalTranslation(
-          translation: Offset(1.0 - _drawerSlideController.value, 0.0),
-          child: _isDrawerClosed() ? const SizedBox() : const MenuView(),
-        );
-      },
     );
   }
 }
--- lib/src/home/views/menu_view.dart
+++ lib/src/home/views/menu_view.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:package_info_plus/package_info_plus.dart';
+import 'package:transsectes_app/src/utils/colors.dart';
 
 class MenuView extends StatefulWidget {
   const MenuView({super.key});
@@ -8,25 +9,7 @@ class MenuView extends StatefulWidget {
   State<MenuView> createState() => _MenuViewState();
 }
 
-class _MenuViewState extends State<MenuView>
-    with SingleTickerProviderStateMixin {
-  static const _menuTitles = [];
-  static const _initialDelayTime = Duration(milliseconds: 50);
-  static const _itemSlideTime = Duration(milliseconds: 250);
-  static const _staggerTime = Duration(milliseconds: 50);
-  static const _buttonDelayTime = Duration(milliseconds: 150);
-  static const _buttonTime = Duration(milliseconds: 500);
-  final _animationDuration = _initialDelayTime +
-      (_staggerTime * _menuTitles.length) +
-      _buttonDelayTime +
-      _buttonTime;
-
-  final List<Interval> _itemSlideIntervals = [];
-
-  // ignore: unused_field
-  late Interval _buttonInterval;
-  late AnimationController _staggeredController;
-
+class _MenuViewState extends State<MenuView> {
   late String appName = '';
   late String version = '';
   late String buildNumber = '';
@@ -34,13 +17,6 @@ class _MenuViewState extends State<MenuView>
   @override
   void initState() {
     super.initState();
-
-    _createAnimationIntervals();
-
-    _staggeredController = AnimationController(
-      vsync: this,
-      duration: _animationDuration,
-    )..forward();
 
     _getPackageInfo();
   }
@@ -55,108 +31,27 @@ class _MenuViewState extends State<MenuView>
     });
   }
 
-  void _createAnimationIntervals() {
-    for (var i = 0; i < _menuTitles.length; ++i) {
-      final startTime = _initialDelayTime + (_staggerTime * i);
-      final endTime = startTime + _itemSlideTime;
-      _itemSlideIntervals.add(
-        Interval(
-          startTime.inMilliseconds / _animationDuration.inMilliseconds,
-          endTime.inMilliseconds / _animationDuration.inMilliseconds,
-        ),
-      );
-    }
-
-    final buttonStartTime =
-        Duration(milliseconds: (_menuTitles.length * 50)) + _buttonDelayTime;
-    final buttonEndTime = buttonStartTime + _buttonTime;
-    _buttonInterval = Interval(
-      buttonStartTime.inMilliseconds / _animationDuration.inMilliseconds,
-      buttonEndTime.inMilliseconds / _animationDuration.inMilliseconds,
-    );
-  }
-
-  @override
-  void dispose() {
-    _staggeredController.dispose();
-
-    super.dispose();
-  }
-
   @override
   Widget build(BuildContext context) {
-    return Container(
-      color: Colors.white,
-      child: Stack(
-        fit: StackFit.expand,
-        children: [
-          _buildLogo(),
-          _buildContent(),
-        ],
-      ),
-    );
-  }
-
-  Widget _buildLogo() {
-    return Container();
-  }
-
-  Widget _buildContent() {
-    return SafeArea(
-      child: Column(
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: [
-          const SizedBox(height: 16),
-          ..._buildListItems(),
-          const Spacer(),
-          Center(
-            child: Text(
-              '$appName - $version($buildNumber)',
-              style: const TextStyle(color: Colors.grey),
-            ),
-          )
-        ],
-      ),
-    );
-  }
-
-  List<Widget> _buildListItems() {
-    final listItems = <Widget>[];
-
-    for (var i = 0; i < _menuTitles.length; ++i) {
-      listItems.add(
-        AnimatedBuilder(
-          animation: _staggeredController,
-          builder: (context, child) {
-            final animationPercent = Curves.easeOut.transform(
-              _itemSlideIntervals[i].transform(_staggeredController.value),
-            );
-            final opacity = animationPercent;
-            final slideDistance = (1.0 - animationPercent) * 150;
-
-            return Opacity(
-              opacity: opacity,
-              child: Transform.translate(
-                offset: Offset(slideDistance, 0),
-                child: child,
-              ),
-            );
-          },
-          child: Padding(
-            padding: const EdgeInsets.symmetric(horizontal: 36, vertical: 16),
-            child: Text(
-              _menuTitles[i],
-              textAlign: TextAlign.left,
-              style: const TextStyle(
-                fontSize: 24,
-                fontWeight: FontWeight.w500,
+    return Drawer(
+      backgroundColor: kColorBackground,
+      child: SafeArea(
+        child: Column(
+          children: [
+            Expanded(
+              child: ListView(
+                children: [],
               ),
             ),
-          ),
+            Center(
+              child: Text(
+                '$appName - $version($buildNumber)',
+                style: const TextStyle(color: Colors.grey),
+              ),
+            )
+          ],
         ),
-      );
-    }
-
-    return listItems;
+      ),
+    );
   }
 }
