a0168cf72072adb301e890dde5b0b487cc37a037
Arnau <61731879+0xArnau@users.noreply.github.com>
2024-05-12 21:16:16 +0200

    chore(menu): remove foreground & background switch

--- lib/src/menu/views/menu_view.dart
+++ lib/src/menu/views/menu_view.dart
@@ -1,13 +1,11 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
-import 'package:go_router/go_router.dart';
 import 'package:logger/logger.dart';
 import 'package:package_info_plus/package_info_plus.dart';
 import 'package:permission_handler/permission_handler.dart';
 import 'package:transsectes_app/src/auth/bloc/auth_bloc.dart';
 import 'package:transsectes_app/src/auth/repositories/auth_repository.dart';
 import 'package:transsectes_app/src/auth/widgets/log_out_widget.dart';
-import 'package:transsectes_app/src/geolocation/bloc/geolocation_bloc.dart';
 import 'package:transsectes_app/src/utils/Widgets/custom_alert_dialog_widget.dart';
 import 'package:transsectes_app/src/utils/colors.dart';
 
@@ -94,56 +92,56 @@ class _MenuViewState extends State<MenuView> {
                       },
                     ),
                   ),
-                  Center(
-                    child: BlocBuilder<GeolocationBloc, GeolocationState>(
-                      builder: (context, state) {
-                        return ListTile(
-                          leading: const Icon(Icons.gps_fixed),
-                          title: const Text("GPS (Foreground)"),
-                          trailing: Switch(
-                            value: state.foregroundPermission,
-                            onChanged: (value) {
-                              Logger().d("value: $value");
-                              if (value) {
-                                context
-                                    .read<GeolocationBloc>()
-                                    .add(RequestForegroundGeolocation());
-                              } else {
-                                openAppSettings();
-                              }
-                            },
-                            activeColor: kColorBackgroundWave,
-                          ),
-                        );
-                      },
-                    ),
-                  ),
-                  Center(
-                    child: BlocBuilder<GeolocationBloc, GeolocationState>(
-                      builder: (context, state) {
-                        return ListTile(
-                          leading: const Icon(Icons.gps_fixed),
-                          title: const Text("GPS (Background)"),
-                          subtitle: const Text(
-                              "Required to get automatically the position"),
-                          trailing: Switch(
-                            value: state.backgroundPermission,
-                            onChanged: (value) {
-                              Logger().d("value: $value");
-                              if (value) {
-                                context
-                                    .read<GeolocationBloc>()
-                                    .add(RequestBackgroundGeolocation());
-                              } else {
-                                openAppSettings();
-                              }
-                            },
-                            activeColor: kColorBackgroundWave,
-                          ),
-                        );
-                      },
-                    ),
-                  ),
+                  // Center(
+                  //   child: BlocBuilder<GeolocationBloc, GeolocationState>(
+                  //     builder: (context, state) {
+                  //       return ListTile(
+                  //         leading: const Icon(Icons.gps_fixed),
+                  //         title: const Text("GPS (Foreground)"),
+                  //         trailing: Switch(
+                  //           value: state.foregroundPermission,
+                  //           onChanged: (value) {
+                  //             Logger().d("value: $value");
+                  //             if (value) {
+                  //               context
+                  //                   .read<GeolocationBloc>()
+                  //                   .add(RequestForegroundGeolocation());
+                  //             } else {
+                  //               openAppSettings();
+                  //             }
+                  //           },
+                  //           activeColor: kColorBackgroundWave,
+                  //         ),
+                  //       );
+                  //     },
+                  //   ),
+                  // ),
+                  // Center(
+                  //   child: BlocBuilder<GeolocationBloc, GeolocationState>(
+                  //     builder: (context, state) {
+                  //       return ListTile(
+                  //         leading: const Icon(Icons.gps_fixed),
+                  //         title: const Text("GPS (Background)"),
+                  //         subtitle: const Text(
+                  //             "Required to get automatically the position"),
+                  //         trailing: Switch(
+                  //           value: state.backgroundPermission,
+                  //           onChanged: (value) {
+                  //             Logger().d("value: $value");
+                  //             if (value) {
+                  //               context
+                  //                   .read<GeolocationBloc>()
+                  //                   .add(RequestBackgroundGeolocation());
+                  //             } else {
+                  //               openAppSettings();
+                  //             }
+                  //           },
+                  //           activeColor: kColorBackgroundWave,
+                  //         ),
+                  //       );
+                  //     },
+                  //   ),
+                  // ),
                   Center(
                     child: ListTile(
                       leading: const Icon(Icons.open_in_new),
