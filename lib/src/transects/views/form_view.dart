75ca30cf661df61e6a18d26d6d24128dc9e00cda
Arnau <61731879+0xArnau@users.noreply.github.com>
2024-06-17 16:42:06 +0200

    fix(Remove/Send data): add a confirmation dialog

--- lib/src/transects/views/form_view.dart
+++ lib/src/transects/views/form_view.dart
@@ -5,6 +5,7 @@ import 'package:transsectes_app/generated/l10n.dart';
 import 'package:transsectes_app/src/geolocation/bloc/geolocation_bloc.dart';
 import 'package:transsectes_app/src/transects/bloc/transect_bloc.dart';
 import 'package:transsectes_app/src/utils/Widgets/counter_button_widget.dart';
+import 'package:transsectes_app/src/utils/Widgets/custom_alert_dialog_widget.dart';
 import 'package:transsectes_app/src/utils/Widgets/custom_buttom_trailing.dart';
 import 'package:transsectes_app/src/utils/Widgets/custom_button.dart';
 import 'package:transsectes_app/src/utils/Widgets/custom_scaffold.dart';
@@ -105,36 +106,64 @@ class _FormViewState extends State<FormView> {
                       children: [
                         customButton(
                           text: S.current.cancel,
-                          onTap: () {
+                          onTap: () async {
                             if (state is TransectStarted) {
-                              context
-                                  .read<TransectBloc>()
-                                  .add(CancelTransect());
+                              await showDialog<String>(
+                                context: context,
+                                builder: (BuildContext _) =>
+                                    CustomAlertDialogWidget(
+                                  content:
+                                      'If you continue, the information will be lost and cannot be recovered.',
+                                  primaryText: 'Delete',
+                                  primaryFunction: () {
+                                    context
+                                        .read<TransectBloc>()
+                                        .add(CancelTransect());
 
-                              context
-                                  .read<GeolocationBloc>()
-                                  .add(ResetGeolocation());
+                                    context
+                                        .read<GeolocationBloc>()
+                                        .add(ResetGeolocation());
+                                    context.pop();
+                                  },
+                                  secondaryText: 'Cancel',
+                                  secondaryFunction: () {},
+                                ),
+                              );
                             }
-
-                            context.pop();
                           },
                         ),
                         BlocBuilder<GeolocationBloc, GeolocationState>(
                           builder: (context, state) {
                             return customButton(
                               text: S.current.send,
-                              onTap: () {
+                              onTap: () async {
                                 if (state is GeolocationLoaded) {
-                                  context.read<TransectBloc>().add(StopTransect(
-                                        tractor: tractor,
-                                        informedPeople: peopleInformed,
-                                        observations: observations.text,
-                                        geolocationModel: state.geolocation,
-                                      ));
-                                  context
-                                      .read<GeolocationBloc>()
-                                      .add(ResetGeolocation());
-                                  context.pop();
+                                  await showDialog<String>(
+                                    context: context,
+                                    builder: (BuildContext _) =>
+                                        CustomAlertDialogWidget(
+                                      content:
+                                          'If you continue, the information will be saved and cannot be modified.',
+                                      primaryText: 'Save',
+                                      primaryFunction: () {
+                                        context
+                                            .read<TransectBloc>()
+                                            .add(StopTransect(
+                                              tractor: tractor,
+                                              informedPeople: peopleInformed,
+                                              observations: observations.text,
+                                              geolocationModel:
+                                                  state.geolocation,
+                                            ));
+                                        context
+                                            .read<GeolocationBloc>()
+                                            .add(ResetGeolocation());
+                                        context.pop();
+                                      },
+                                      secondaryText: 'Cancel',
+                                      secondaryFunction: () {},
+                                    ),
+                                  );
                                 }
                               },
                             );
