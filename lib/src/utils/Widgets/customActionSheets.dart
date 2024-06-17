d015d6ff8aa42290a58da05aa4b19e76e881023b
Arnau <61731879+0xArnau@users.noreply.github.com>
2024-06-17 10:38:46 +0200

    feat(customActionSheet): remove background blur

--- lib/src/utils/Widgets/customActionSheets.dart
+++ lib/src/utils/Widgets/customActionSheets.dart
@@ -1,5 +1,3 @@
-import 'dart:ui'; // Necesario para ImageFilter.blur
-
 import 'package:flutter/material.dart';
 import 'package:transsectes_app/src/utils/colors.dart';
 
@@ -25,149 +23,134 @@ Future customActionSheet({
     backgroundColor: Colors.transparent,
     context: context,
     builder: (context) {
-      return Stack(
-        children: [
-          Positioned.fill(
-            child: BackdropFilter(
-              filter: ImageFilter.blur(
-                sigmaX: 10,
-                sigmaY: 10,
+      return Container(
+        margin: const EdgeInsets.only(left: 10, right: 10, bottom: 50),
+        child: Column(
+          mainAxisSize: MainAxisSize.min,
+          children: <Widget>[
+            Container(
+              decoration: BoxDecoration(
+                color: Colors.grey[350],
+                borderRadius: const BorderRadius.only(
+                  topLeft: Radius.circular(15),
+                  topRight: Radius.circular(15),
+                  bottomLeft: Radius.circular(15),
+                  bottomRight: Radius.circular(15),
+                ),
+              ),
+              child: Column(
+                children: [
+                  if (title != null || description != null) ...[
+                    Container(
+                      padding: const EdgeInsets.fromLTRB(0, 10, 0, 0),
+                      child: Column(
+                        children: <Widget>[
+                          if (title != null) ...[
+                            Text(
+                              title,
+                              style: const TextStyle(
+                                fontWeight: FontWeight.bold,
+                                fontSize: 18,
+                              ),
+                            ),
+                          ],
+                          if (description != null) ...[
+                            const SizedBox(height: 8),
+                            Text(
+                              description,
+                              style: const TextStyle(
+                                color: Colors.grey,
+                              ),
+                            ),
+                          ],
+                          const SizedBox(height: 8),
+                          const Divider(height: 0),
+                        ],
+                      ),
+                    ),
+                  ],
+                  ListTile(
+                    title: Text(
+                      primaryAction.text,
+                      textAlign: TextAlign.center,
+                      style: const TextStyle(
+                        color: Colors.red,
+                        fontWeight: FontWeight.w600,
+                      ),
+                    ),
+                    onTap: () {
+                      primaryAction.function();
+                      Navigator.pop(context);
+                      // Add your action code here
+                    },
+                  ),
+                  if (secondaryAction != null) ...[
+                    const Divider(height: 0),
+                    ListTile(
+                      title: Text(
+                        primaryAction.text,
+                        textAlign: TextAlign.center,
+                        style: const TextStyle(
+                          color: kColorTitle,
+                          fontWeight: FontWeight.w600,
+                        ),
+                      ),
+                      onTap: () {
+                        secondaryAction.function();
+                        Navigator.pop(context);
+                        // Add your action code here
+                      },
+                    ),
+                  ],
+                  if (tertiaryAction != null) ...[
+                    const Divider(height: 0),
+                    ListTile(
+                      title: Text(
+                        primaryAction.text,
+                        textAlign: TextAlign.center,
+                        style: const TextStyle(
+                          color: kColorTitle,
+                          fontWeight: FontWeight.w600,
+                        ),
+                      ),
+                      onTap: () {
+                        tertiaryAction.function();
+                        Navigator.pop(context);
+                        // Add your action code here
+                      },
+                    ),
+                  ],
+                ],
+              ),
+            ),
+            const SizedBox(height: 10),
+            ElevatedButton(
+              onPressed: () {
+                Navigator.pop(context);
+                // Add your action code here
+              },
+              style: ElevatedButton.styleFrom(
+                foregroundColor: kColorTitle,
+                backgroundColor: Colors.grey[350],
+                shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(15),
+                  side: BorderSide(color: Colors.grey.shade200),
+                ),
               ),
               child: Container(
-                color: Colors.transparent,
+                alignment: Alignment.center,
+                height: 50,
+                child: const Text(
+                  'Cancel',
+                  style: TextStyle(
+                    fontWeight: FontWeight.bold,
+                    fontSize: 18,
+                  ),
+                ),
               ),
             ),
-          ),
-          Container(
-            margin: const EdgeInsets.only(left: 10, right: 10, bottom: 50),
-            child: Column(
-              mainAxisSize: MainAxisSize.min,
-              children: <Widget>[
-                Container(
-                  decoration: BoxDecoration(
-                    color: Colors.white.withOpacity(0.8),
-                    borderRadius: const BorderRadius.only(
-                      topLeft: Radius.circular(15),
-                      topRight: Radius.circular(15),
-                      bottomLeft: Radius.circular(15),
-                      bottomRight: Radius.circular(15),
-                    ),
-                  ),
-                  child: Column(
-                    children: [
-                      if (title != null || description != null) ...[
-                        Container(
-                          padding: const EdgeInsets.fromLTRB(0, 10, 0, 0),
-                          child: Column(
-                            children: <Widget>[
-                              if (title != null) ...[
-                                Text(
-                                  title,
-                                  style: const TextStyle(
-                                    fontWeight: FontWeight.bold,
-                                    fontSize: 18,
-                                  ),
-                                ),
-                              ],
-                              if (description != null) ...[
-                                const SizedBox(height: 8),
-                                Text(
-                                  description,
-                                  style: const TextStyle(
-                                    color: Colors.grey,
-                                  ),
-                                ),
-                              ],
-                              const SizedBox(height: 8),
-                              const Divider(height: 0),
-                            ],
-                          ),
-                        ),
-                      ],
-                      ListTile(
-                        title: Text(
-                          primaryAction.text,
-                          textAlign: TextAlign.center,
-                          style: const TextStyle(
-                            color: Colors.red,
-                            fontWeight: FontWeight.w600,
-                          ),
-                        ),
-                        onTap: () {
-                          primaryAction.function();
-                          Navigator.pop(context);
-                          // Add your action code here
-                        },
-                      ),
-                      if (secondaryAction != null) ...[
-                        const Divider(height: 0),
-                        ListTile(
-                          title: Text(
-                            primaryAction.text,
-                            textAlign: TextAlign.center,
-                            style: const TextStyle(
-                              color: kColorTitle,
-                              fontWeight: FontWeight.w600,
-                            ),
-                          ),
-                          onTap: () {
-                            secondaryAction.function();
-                            Navigator.pop(context);
-                            // Add your action code here
-                          },
-                        ),
-                      ],
-                      if (tertiaryAction != null) ...[
-                        const Divider(height: 0),
-                        ListTile(
-                          title: Text(
-                            primaryAction.text,
-                            textAlign: TextAlign.center,
-                            style: const TextStyle(
-                              color: kColorTitle,
-                              fontWeight: FontWeight.w600,
-                            ),
-                          ),
-                          onTap: () {
-                            tertiaryAction.function();
-                            Navigator.pop(context);
-                            // Add your action code here
-                          },
-                        ),
-                      ],
-                    ],
-                  ),
-                ),
-                const SizedBox(height: 10),
-                ElevatedButton(
-                  onPressed: () {
-                    Navigator.pop(context);
-                    // Add your action code here
-                  },
-                  style: ElevatedButton.styleFrom(
-                    foregroundColor: kColorTitle,
-                    backgroundColor: Colors.white.withOpacity(0.8),
-                    shape: RoundedRectangleBorder(
-                      borderRadius: BorderRadius.circular(15),
-                      side: BorderSide(color: Colors.grey.shade200),
-                    ),
-                  ),
-                  child: Container(
-                    alignment: Alignment.center,
-                    height: 50,
-                    child: const Text(
-                      'Cancel',
-                      style: TextStyle(
-                        fontWeight: FontWeight.bold,
-                        fontSize: 18,
-                      ),
-                    ),
-                  ),
-                ),
-              ],
-            ),
-          ),
-        ],
+          ],
+        ),
       );
     },
   );
