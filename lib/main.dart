d36eb400636e3112588adf85a477e977d938a1d3
Arnau <61731879+0xArnau@users.noreply.github.com>
2024-12-21 11:36:52 +0100

    feat(main): add `dark`/`light` mode

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,21 @@
+import 'package:flutter/material.dart';
+import 'package:transsectes_app/app/features/splash/presentation/views/splash_view.dart';
+import 'package:transsectes_app/app/theme/theme.dart';
 
 void main() {
+  runApp(const MainApp());
+}
+
+class MainApp extends StatelessWidget {
+  const MainApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: const SplashView(),
+      theme: lightMode,
+      darkTheme: darkMode,
+    );
+  }
 }
