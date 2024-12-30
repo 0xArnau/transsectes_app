c8190a91e20fe6a5a77bfe1a7d3938e93004f1d6
Arnau <61731879+0xArnau@users.noreply.github.com>
2024-12-30 20:35:38 +0100

    wip(RemoveTransects): add UI
    TODO: add logic to remove it from database

--- lib/app/features/transects/presentation/views/records/remove_transects_view.dart
+++ lib/app/features/transects/presentation/views/records/remove_transects_view.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:transsectes_app/app/core/widgets/custom_button.dart';
+import 'package:transsectes_app/generated/l10n.dart';
 
 class RemoveTransectsView extends ConsumerStatefulWidget {
   const RemoveTransectsView({super.key});
@@ -12,6 +14,59 @@ class RemoveTransectsView extends ConsumerStatefulWidget {
 class _RemoveTransectsViewState extends ConsumerState<RemoveTransectsView> {
   @override
   Widget build(BuildContext context) {
-    return const Text('RemoveTransectsView');
+    return Center(
+      child: Container(
+        height: 85,
+        padding: const EdgeInsets.all(16.0),
+        child: CustomButton(
+          text: S.current.delete_all_transects,
+          onTap: _showConfirmationDialog,
+          isADestructiveAction: true,
+          minWidth: 50,
+        ),
+      ),
+    );
+  }
+
+  // Function to show the confirmation dialog
+  Future<void> _showConfirmationDialog() async {
+    return showDialog<void>(
+      context: context,
+      barrierDismissible: false,
+      builder: (BuildContext context) {
+        return AlertDialog(
+          title: Text(S.current.delete_all_transects),
+          content: Text(S.current.delete_all_transects_content),
+          actions: <Widget>[
+            TextButton(
+              onPressed: () {
+                Navigator.of(context).pop();
+              },
+              child: Text(S.current.cancel),
+            ),
+            TextButton(
+              onPressed: () {
+                Navigator.of(context).pop();
+                _deleteAllTransects();
+              },
+              child: Text(
+                S.current.remove,
+                style: TextStyle(color: Theme.of(context).colorScheme.error),
+              ),
+            ),
+          ],
+        );
+      },
+    );
+  }
+
+  // Function to handle the deletion logic (can be replaced with actual deletion logic)
+  void _deleteAllTransects() {
+    // Replace this with your actual deletion logic
+    ScaffoldMessenger.of(context).showSnackBar(
+      const SnackBar(
+        content: Text('All transects deleted successfully!'), // TODO: i18n
+      ),
+    );
   }
 }
