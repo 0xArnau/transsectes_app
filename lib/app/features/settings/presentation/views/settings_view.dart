58246f6e8ca8941d8877d6505eea528a9d653cc2
Arnau <61731879+0xArnau@users.noreply.github.com>
2025-06-21 12:08:27 +0200

    feat(counter_button_widget): hide widget from semantic tree

--- lib/app/features/settings/presentation/views/settings_view.dart
+++ lib/app/features/settings/presentation/views/settings_view.dart
@@ -60,13 +60,15 @@ class _SettingsViewState extends ConsumerState<SettingsView> {
         ],
       ),
       bottomNavigationBar: BottomAppBar(
+          child: Semantics(
+        hidden: true,
         child: Image.asset(
           Theme.of(context).brightness == Brightness.dark
               ? 'assets/imgs/logo/GEPEC_EdC_OFICIAL_blanc.png'
               : 'assets/imgs/logo/GEPEC_EdC_OFICIAL.png',
           fit: BoxFit.contain,
         ),
-      ),
+      )),
       body: Padding(
         padding: const EdgeInsets.only(left: 16.0, right: 16.0),
         child: Column(
