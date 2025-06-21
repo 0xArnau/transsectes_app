2cb7d4a5a17108c7261234a25c8f331dddc0a80e
Arnau <61731879+0xArnau@users.noreply.github.com>
2025-06-21 11:42:48 +0200

    wip(sign_up_view): use `Semantics` to add `label` to checkboxes

--- lib/app/features/auth/presentation/views/sign_up_view.dart
+++ lib/app/features/auth/presentation/views/sign_up_view.dart
@@ -183,6 +183,7 @@ class _SignUpViewState extends ConsumerState<SignUpView> {
           },
           text: S.current.legalNotice,
           path: 'assets/docs/legal/avis-legal.pdf',
+          value: _avisLegal,
         ),
         _checkboxPdfTile(
           context: context,
@@ -196,6 +197,7 @@ class _SignUpViewState extends ConsumerState<SignUpView> {
           },
           text: S.current.informativeClause,
           path: 'assets/docs/legal/clausula-informativa.pdf',
+          value: _clausulaInformativa,
         ),
         _checkboxPdfTile(
           context: context,
@@ -209,6 +211,7 @@ class _SignUpViewState extends ConsumerState<SignUpView> {
           },
           text: S.current.privacy,
           path: 'assets/docs/legal/privacitat.pdf',
+          value: _privacitat,
         ),
         const SizedBox(height: 16),
         CustomButton(
@@ -235,12 +238,17 @@ class _SignUpViewState extends ConsumerState<SignUpView> {
     required Function(bool) onChanged,
     required String text,
     required String path,
+    required bool value,
   }) {
     return ListTile(
-      leading: Checkbox(
-        activeColor: Theme.of(context).colorScheme.primary,
-        value: checkbox,
-        onChanged: (value) => onChanged(value ?? false),
+      leading: Semantics(
+        label: text,
+        value: value ? 'Marcado' : 'Desmarcado',
+        child: Checkbox(
+          activeColor: Theme.of(context).colorScheme.primary,
+          value: checkbox,
+          onChanged: (value) => onChanged(value ?? false),
+        ),
       ),
       title: TextButton(
         onPressed: () {
